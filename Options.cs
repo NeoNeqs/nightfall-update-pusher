14b99c6a312105a65710819d406552ed7209b6ae
NeqsTech <michael0march@protonmail.com>
2022-05-15 00:43:53 +0200

    Add missing HelpText field for -v (--verbose) option

--- Options.cs
+++ Options.cs
@@ -7,7 +7,7 @@ public sealed class Options
     [Option('p', "path", Required = true, HelpText = "Sets the path to push updates on/from.")]
     public string Path { get; set; } = null!;
 
-    [Option('v', "verbose", Required = false, HelpText = "")]
+    [Option('v', "verbose", Required = false, HelpText = "Sets whether to print the stacktrace of an exception.")]
     public bool Verbose { get; set; } = false;
 
 }
